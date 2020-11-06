8a10ea90b134d831f2f491e3c4550446b29783c5
jt5493 <jt5493@hotmail.com>
2020-11-06 09:53:30 +0900

    added create route to binder contr

--- app/controllers/api/v1/binders_controller.rb
+++ app/controllers/api/v1/binders_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::BindersController < ApplicationController
+
+
+    def index
+        @binders = Binder.all
+        render json: @binders
+    end
+
+
+
+    def create
+        binder = Binder.new(binder_params)
+        if binder.save
+            render json: binder, status: :accepted
+        else
+            render json: {errors: binder.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+
+
+
+
+
+    private
+
+    def binder_params
+        params.require(:binder).permit(
+            :title,
+            :created_at
+        )
+    end
+
+
 end
