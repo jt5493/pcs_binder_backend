74e95c69363f76400dd2d8b05bc23eef08b9c817
jt5493 <jt5493@hotmail.com>
2020-11-12 12:34:48 +0900

    added relationships to binder serializer

--- app/serializers/binder_serializer.rb
+++ app/serializers/binder_serializer.rb
@@ -1,4 +1,7 @@
 class BinderSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :created_at, :notes, :documents, :links
+  has_many :documents
+  has_many :notes
+  has_many :links
 end
